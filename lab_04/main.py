81e2a5846b71994af92ae3921ae62734b8b2a120
Aramis2424 <srd2424@yandex.ru>
2022-04-19 00:23:31 +0300

    made interface

--- lab_04/main.py
+++ lab_04/main.py
@@ -20,13 +20,13 @@ dots_for_line = []
 dots = []
 last_activity = []
 
-TASK = '''Реализовать построение окружности (эллипса) методами: \n
+TASK = '''Реализовать алгоритмы построения окружности и эллипса методами: \n
 - Канонического уравнения\n
 - Параметрического уравнения\n
 - Алгоритма Брезенхема\n
 - Алгоритма средней точки\n
 Построить спектр окружностей и эллипсов.\n
-Замер эффективности по времени.'''
+Замерить время работы алгоритмов.'''
 
 
 def del_all_dots():
@@ -48,110 +48,183 @@ def main():
                               bg='#6b7a0a')
     label_menu.place(relx=0, rely=0, relwidth=0.3, relheight=0.04)
 
-##    #Выбор метода; Установка combox`a
-##    combostyle = ttk.Style() #стиль для Combox`a
-##    combostyle.theme_create('combostyle', parent='alt',
-##                         settings = {'TCombobox':
-##                                     {'configure':
-##                                      {'selectbackground': '#6b7a0a',
-##                                       'fieldbackground': '#6b7a0a',
-##                                       'background': '#6b7a0a'
-##                                       }}}
-##                           )
-##    combostyle.theme_use('combostyle')
-##
-##    label_method = Label(text="Метод:", bg="#6b7a0a", anchor='w')
-##    label_method.place(relx=0, rely=0.07, relwidth=0.3, relheight=0.04)
-##
-##    method_list = ("ЦДА", "Брезенхем (вещественный)",
-##                   "Брезенхем (целочисленный)",
-##                   "Брезенхем (сглаживание)", "Ву")
-##    font_combo = ("Times", 12)
-##
-##    method_combo = ttk.Combobox(root, state='readonly', values=method_list,
-##                    font=font_combo)
-##    method_combo.place(relx=0, rely=0.11, relwidth=0.3, relheight=0.06)
-##    method_combo.current(0)
-##
-##    #Задание отрезка
-##    label_shift = Label(root, text="Отрезок:", anchor='w',
-##                              bg='#6b7a0a')
-##    label_shift.place(relx=0, rely=0.23, relwidth=0.3, relheight=0.04)
-##
-##    #Цвет
-##    color_list = ("Черный", "Белый", "Красный",
-##                   "Синий", "Зеленый")
-##    label_color = Label(root, text="Цвет:", anchor='w',
-##                              bg='#6b7a0a')
-##    label_color.place(relx=0, rely=0.275, relwidth=0.1, relheight=0.06)
-##    color_combo = ttk.Combobox(root, state='readonly', values=color_list,
-##                    font=font_combo)
-##    color_combo.place(relx=0.1, rely=0.275, relwidth=0.2, relheight=0.06)
-##    color_combo.current(0)
-##
-##    #Координаты отрезка
-##    label_x1 = Label(root, text="X1:", anchor='c',
-##                              bg='#6b7a0a')
-##    label_x1.place(relx=0, rely=0.35, relwidth=0.04, relheight=0.06)
-##    line_x1 = Entry(root, bg='#6b7a0a')
-##    line_x1.place(relx=0.045, rely=0.35, relwidth=0.04, relheight=0.06)
-##
-##    label_y1 = Label(root, text="Y1:", anchor='c',
-##                              bg='#6b7a0a')
-##    label_y1.place(relx=0.15, rely=0.35, relwidth=0.04, relheight=0.06)
-##    line_y1 = Entry(root, bg='#6b7a0a')
-##    line_y1.place(relx=0.195, rely=0.35, relwidth=0.04, relheight=0.06)
-##
-##    label_x2 = Label(root, text="X2:", anchor='c',
-##                              bg='#6b7a0a')
-##    label_x2.place(relx=0, rely=0.415, relwidth=0.04, relheight=0.06)
-##    line_x2 = Entry(root, bg='#6b7a0a')
-##    line_x2.place(relx=0.045, rely=0.415, relwidth=0.04, relheight=0.06)
-##
-##    label_y2 = Label(root, text="Y2:", anchor='c',
-##                              bg='#6b7a0a')
-##    label_y2.place(relx=0.15, rely=0.415, relwidth=0.04, relheight=0.06)
-##    line_y2 = Entry(root, bg='#6b7a0a')
-##    line_y2.place(relx=0.195, rely=0.415, relwidth=0.04, relheight=0.06)
-##
-##    line_btn = Button(text="Построить отрезок",
-##                  bg='#6b7a0a',
-##                  activebackground='#6b7a0a',
-##                  command=lambda: drawLine([line_x1.get(), line_y1.get()],
-##                  [line_x2.get(), line_y2.get()], color_combo.current(),
-##                   method_combo.current()))
-##    line_btn.place(relx=0, rely=0.485, relwidth=0.3, relheight=0.08)
-##
-##    #Спектр
-##    menu_label_spectr = Label(text="Спектр:", anchor='w', bg='#6b7a0a')
-##    menu_label_spectr.place(relx=0, rely=0.6, relwidth=0.3, relheight=0.05)
-##
-##    label_len = Label(root, text="Длина:", anchor='c',
-##                              bg='#6b7a0a')
-##    label_len.place(relx=0, rely=0.655, relwidth=0.15, relheight=0.06)
-##    len_spectr = Entry(root, bg='#6b7a0a')
-##    len_spectr.place(relx=0.155, rely=0.655, relwidth=0.14, relheight=0.06)
-##
-##    label_angle = Label(root, text="Угол:", anchor='c',
-##                             bg='#6b7a0a')
-##    label_angle.place(relx=0, rely=0.722, relwidth=0.15, relheight=0.06)
-##    angle_spectr = Entry(root, bg='#6b7a0a')
-##    angle_spectr.place(relx=0.155, rely=0.722, relwidth=0.14, relheight=0.06)
-##
-##    spectr_btn = Button(text="Построить спектр",
-##                      bg='#6b7a0a',
-##                      activebackground='#6b7a0a',
-##                      command=lambda: drawSpectr(len_spectr.get(),
-##                      color_combo.current(), angle_spectr.get(),
-##                      method_combo.current()))
-##    spectr_btn.place(relx=0, rely=0.79, relwidth=0.3, relheight=0.08)
-##
-##    #Диаграмма
-##    plt_1_btn = Button(text="Диаграмма ступенчатости", bg='#6b7a0a',
-##                    activebackground='#6b7a0a',
-##                    command=lambda: step_diagram(len_spectr.get()))
-##
-##    plt_1_btn.place(relx=0, rely=0.91, relwidth=0.3, relheight=0.08)
+    #Выбор метода; Установка combox`a
+    combostyle = ttk.Style() #стиль для Combox`a
+    combostyle.theme_create('combostyle', parent='alt',
+                         settings = {'TCombobox':
+                                     {'configure':
+                                      {'selectbackground': '#6b7a0a',
+                                       'fieldbackground': '#6b7a0a',
+                                       'background': '#6b7a0a'
+                                       }}}
+                           )
+    combostyle.theme_use('combostyle')
+
+    label_method = Label(text="Метод:", bg="#6b7a0a", anchor='w')
+    label_method.place(relx=0, rely=0.06, relwidth=0.3, relheight=0.04)
+
+    method_list = ("Каноническое уравнение", "Параметрическое уравнение",
+                   "Алгоритм Брезенхема",
+                   "Алгоритм средней точки", "Библиотечноая функция")
+    font_combo = ("Times", 12)
+
+    method_combo = ttk.Combobox(root, state='readonly', values=method_list,
+                    font=font_combo)
+    method_combo.place(relx=0, rely=0.1, relwidth=0.3, relheight=0.06)
+    method_combo.current(0)
+
+    #Цвет
+    color_list = ("Черный", "Белый", "Красный",
+                   "Синий", "Зеленый")
+    label_color = Label(root, text="Цвет:", anchor='w',
+                              bg='#6b7a0a')
+    label_color.place(relx=0, rely=0.162, relwidth=0.1, relheight=0.06)
+    color_combo = ttk.Combobox(root, state='readonly', values=color_list,
+                    font=font_combo)
+    color_combo.place(relx=0.1, rely=0.162, relwidth=0.2, relheight=0.06)
+    color_combo.current(0)
+
+    #Задание окружности
+    label_shift = Label(root, text="Окружность:", anchor='w',
+                              bg='#6b7a0a')
+    label_shift.place(relx=0, rely=0.245, relwidth=0.3, relheight=0.04)
+    #Координаты окружности
+    label_x1 = Label(root, text="X:", anchor='c',
+                              bg='#6b7a0a')
+    label_x1.place(relx=0, rely=0.29, relwidth=0.04, relheight=0.06)
+    line_x1 = Entry(root, bg='#6b7a0a')
+    line_x1.place(relx=0.043, rely=0.29, relwidth=0.04, relheight=0.06)
+
+    label_y1 = Label(root, text="Y:", anchor='c',
+                              bg='#6b7a0a')
+    label_y1.place(relx=0.1, rely=0.29, relwidth=0.04, relheight=0.06)
+    line_y1 = Entry(root, bg='#6b7a0a')
+    line_y1.place(relx=0.143, rely=0.29, relwidth=0.04, relheight=0.06)
+
+    label_rc = Label(root, text="R:", anchor='c',
+                              bg='#6b7a0a')
+    label_rc.place(relx=0.2, rely=0.29, relwidth=0.04, relheight=0.06)
+    line_rc = Entry(root, bg='#6b7a0a')
+    line_rc.place(relx=0.243, rely=0.29, relwidth=0.04, relheight=0.06)
+
+    cir_btn = Button(text="Построить окружность",
+                  bg='#6b7a0a',
+                  activebackground='#6b7a0a')#,
+                  #command=lambda: drawLine([line_x1.get(), line_y1.get()],
+                  #[line_x2.get(), line_y2.get()], color_combo.current(),
+                  #method_combo.current()))
+    cir_btn.place(relx=0, rely=0.36, relwidth=0.3, relheight=0.05)
+
+    #Задание эллипса
+    label_shift = Label(root, text="Эллипс:", anchor='w',
+                              bg='#6b7a0a')
+    label_shift.place(relx=0, rely=0.44, relwidth=0.4, relheight=0.04)
+    #Координаты эллипса
+    label_x2 = Label(root, text="X:", anchor='c',
+                              bg='#6b7a0a')
+    label_x2.place(relx=0, rely=0.485, relwidth=0.03, relheight=0.06)
+    line_x2 = Entry(root, bg='#6b7a0a')
+    line_x2.place(relx=0.033, rely=0.485, relwidth=0.03, relheight=0.06)
+
+    label_y2 = Label(root, text="Y:", anchor='c',
+                              bg='#6b7a0a')
+    label_y2.place(relx=0.075, rely=0.485, relwidth=0.03, relheight=0.06)
+    line_y2 = Entry(root, bg='#6b7a0a')
+    line_y2.place(relx=0.108, rely=0.485, relwidth=0.03, relheight=0.06)
+
+    label_ra = Label(root, text="Ra:", anchor='c',
+                              bg='#6b7a0a')
+    label_ra.place(relx=0.155, rely=0.485, relwidth=0.03, relheight=0.06)
+    line_ra = Entry(root, bg='#6b7a0a')
+    line_ra.place(relx=0.188, rely=0.485, relwidth=0.03, relheight=0.06)
+
+    label_rb = Label(root, text="Rb:", anchor='c',
+                              bg='#6b7a0a')
+    label_rb.place(relx=0.231, rely=0.485, relwidth=0.03, relheight=0.06)
+    line_rb = Entry(root, bg='#6b7a0a')
+    line_rb.place(relx=0.264, rely=0.485, relwidth=0.03, relheight=0.06)
+
+    ell_btn = Button(text="Построить эллипс",
+                  bg='#6b7a0a',
+                  activebackground='#6b7a0a')#,
+                  #command=lambda: drawLine([line_x1.get(), line_y1.get()],
+                  #[line_x2.get(), line_y2.get()], color_combo.current(),
+                  #method_combo.current()))
+    ell_btn.place(relx=0, rely=0.555, relwidth=0.3, relheight=0.05)
+
+    #Спектр окружности
+    label_shift = Label(root, text="Спектр окружности:", anchor='w',
+                              bg='#6b7a0a')
+    label_shift.place(relx=0, rely=0.64, relwidth=0.4, relheight=0.04)
+    #Данные спектра
+    label_xspec_c = Label(root, text="Шаг:", anchor='c',
+                              bg='#6b7a0a')
+    label_xspec_c.place(relx=0, rely=0.685, relwidth=0.03, relheight=0.06)
+    line_xspec_c = Entry(root, bg='#6b7a0a')
+    line_xspec_c.place(relx=0.033, rely=0.685, relwidth=0.03, relheight=0.06)
+
+    label_yspec_c = Label(root, text="N:", anchor='c',
+                              bg='#6b7a0a')
+    label_yspec_c.place(relx=0.075, rely=0.685, relwidth=0.03, relheight=0.06)
+    line_yspec_c = Entry(root, bg='#6b7a0a')
+    line_yspec_c.place(relx=0.108, rely=0.685, relwidth=0.03, relheight=0.06)
+
+    label_rspec_cb = Label(root, text="Нач:", anchor='c',
+                              bg='#6b7a0a')
+    label_rspec_cb.place(relx=0.155, rely=0.685, relwidth=0.03, relheight=0.06)
+    line_rspec_cb = Entry(root, bg='#6b7a0a')
+    line_rspec_cb.place(relx=0.188, rely=0.685, relwidth=0.03, relheight=0.06)
+
+    label_rspec_cf = Label(root, text="Кон:", anchor='c',
+                              bg='#6b7a0a')
+    label_rspec_cf.place(relx=0.231, rely=0.685, relwidth=0.03, relheight=0.06)
+    line_rspec_cf = Entry(root, bg='#6b7a0a')
+    line_rspec_cf.place(relx=0.264, rely=0.685, relwidth=0.03, relheight=0.06)
+
+    spec_c_btn = Button(text="Построить спектр окружности",
+                  bg='#6b7a0a',
+                  activebackground='#6b7a0a')#,
+                  #command=lambda: drawLine([line_x1.get(), line_y1.get()],
+                  #[line_x2.get(), line_y2.get()], color_combo.current(),
+                  #method_combo.current()))
+    spec_c_btn.place(relx=0, rely=0.755, relwidth=0.3, relheight=0.05)
+
+    #Спектр эллипса
+    label_shift = Label(root, text="Спектр эллипса:", anchor='w',
+                              bg='#6b7a0a')
+    label_shift.place(relx=0, rely=0.835, relwidth=0.4, relheight=0.04)
+    #Данные спектра
+    label_xspec_c = Label(root, text="Шаг:", anchor='c',
+                              bg='#6b7a0a')
+    label_xspec_c.place(relx=0, rely=0.88, relwidth=0.03, relheight=0.06)
+    line_xspec_c = Entry(root, bg='#6b7a0a')
+    line_xspec_c.place(relx=0.033, rely=0.88, relwidth=0.03, relheight=0.06)
+
+    label_yspec_c = Label(root, text="N:", anchor='c',
+                              bg='#6b7a0a')
+    label_yspec_c.place(relx=0.075, rely=0.88, relwidth=0.03, relheight=0.06)
+    line_yspec_c = Entry(root, bg='#6b7a0a')
+    line_yspec_c.place(relx=0.108, rely=0.88, relwidth=0.03, relheight=0.06)
+
+    label_rspec_c = Label(root, text="Нач:", anchor='c',
+                              bg='#6b7a0a')
+    label_rspec_cb.place(relx=0.155, rely=0.88, relwidth=0.03, relheight=0.06)
+    line_rspec_cb = Entry(root, bg='#6b7a0a')
+    line_rspec_cb.place(relx=0.188, rely=0.88, relwidth=0.03, relheight=0.06)
+
+    label_rspec_cf = Label(root, text="Кон:", anchor='c',
+                              bg='#6b7a0a')
+    label_rspec_cf.place(relx=0.231, rely=0.88, relwidth=0.03, relheight=0.06)
+    line_rspec_cf = Entry(root, bg='#6b7a0a')
+    line_rspec_cf.place(relx=0.264, rely=0.88, relwidth=0.03, relheight=0.06)
+
+    spec_c_btn = Button(text="Построить спектр эллипса",
+                  bg='#6b7a0a',
+                  activebackground='#6b7a0a')#,
+                  #command=lambda: drawLine([line_x1.get(), line_y1.get()],
+                  #[line_x2.get(), line_y2.get()], color_combo.current(),
+                  #method_combo.current()))
+    spec_c_btn.place(relx=0, rely=0.945, relwidth=0.3, relheight=0.05)
 
     #Canvas
     canvas = Canvas(root, bg="#148012", #148012
@@ -169,7 +242,9 @@ def main():
                         messagebox.showinfo("Автор", "Симонович Р.Д. ИУ7-44Б"))
     menu.add_command(label="Очистить холст", command=lambda:\
                         del_all_dots())
-    #menu.add_command(label="Построить с шагом")#, command=special_add)
+    #Диаграмма
+    menu.add_command(label="Диаграмма", command=lambda:\
+                        time_diagram(len_spectr.get()))
     menu.add_command(label="Выход", command=root.destroy)
 
     #Команды
